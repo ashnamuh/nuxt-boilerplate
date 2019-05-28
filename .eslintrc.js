1d8f4e2614a1431a6bcaf133e93156659d460bd0
Jaemok <ashnamuh@gmail.com>
2019-05-28 12:04:22 +0900

    revise eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const offOnDev = (process.env.NODE_ENV === 'production') ? 'error' : 'off'
+
 module.exports = {
   root: true,
   env: {
@@ -13,5 +15,29 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'no-console': offOnDev,
+    "vue/order-in-components": ["error", {
+      "order": [
+        "el",
+        "name",
+        "parent",
+        "functional",
+        ["delimiters", "comments"],
+        ["components", "directives", "filters"],
+        "extends",
+        "mixins",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "asyncData",
+        "data",
+        "computed",
+        "watch",
+        "LIFECYCLE_HOOKS",
+        "methods",
+        ["template", "render"],
+        "renderError"
+      ]
+    }]
   }
 }
