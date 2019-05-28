2d1f2fa77a9b778e3b11619990f8d6b794ba1e6d
Jaemok <ashnamuh@gmail.com>
2019-05-28 12:00:25 +0900

    set up some nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,29 @@
-import pkg from './package'
+import ConfigWebpackPlugin from 'config-webpack'
+import config from 'config'
 
 export default {
   mode: 'universal',
-
+  server: {
+    port: config.server.port,
+    host: config.server.host
+  },
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'title',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'title', name: 'title', content: 'title' },
+      { hid: 'description', name: 'description', content: 'description' },
+      { hid: 'keywords', name: 'keywords', content: 'keywords' },
+      { hid: 'og:type', property: 'og:type', content: 'og:type' },
+      { hid: 'og:title', property: 'og:title', content: 'og:title' },
+      { hid: 'og:description', property: 'og:description', content: 'og:description' },
+      { hid: 'og:image', property: 'og:image', content: '/path/to/image' },
+      { hid: 'og:image:alt', property: 'og:image:alt', content: 'og:image:alt' },
+      { hid: 'og:url', property: 'og:url', content: 'og:url' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -54,6 +66,10 @@ export default {
   ** Build configuration
   */
   build: {
+    extractCSS: process.env.NODE_ENV === 'production',
+    plugins: [
+      new ConfigWebpackPlugin()
+    ],
     /*
     ** You can extend webpack config here
     */
