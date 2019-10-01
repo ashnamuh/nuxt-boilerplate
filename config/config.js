4ce62da3971dc8020b6b5308106af242e4cb343f
Jaemok <ashnamuh@gmail.com>
2019-10-01 16:06:35 +0900

    add config.js

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const serverless = {
+  stage: 'dev',
+  warmup: true,
+  NODE_ENV: process.env.NODE_ENV || 'development'
+}
+
+const server = {
+  port: 3002,
+  baseUrl: (process.env.NODE_ENV === 'production') ? `/${serverless.stage}/` : '/'
+}
+
+const api = {
+  url: 'http://localhost:8080'
+}
+
+console.log(process.env)
+
+module.exports = {
+  serverless,
+  server,
+  api
+}
